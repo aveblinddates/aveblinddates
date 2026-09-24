619df66fdd965643b74622b9c8e7894a3460a9d0
aveblinddates <ave.blinddates@gmail.com>
2026-09-23 21:57:11 -0400

    Enhance service worker with offline support
    Add offline caching for the service worker and improve activation logic.

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
+const CACHE_NAME = "ave-test-v1";
+
 self.addEventListener("install", event => {
+    console.log("SW INSTALL");
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.add("./offline.html"))
+    );
+
     self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames =>
-            Promise.all(
-                cacheNames.map(cacheName =>
-                    caches.delete(cacheName)
-                )
-            )
-        )
-    );
+    console.log("SW ACTIVATE");
 
-    self.registration.unregister();
+    event.waitUntil(
+        self.clients.claim()
+    );
 });
 
 self.addEventListener("fetch", event => {
-    return;
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            fetch(event.request).catch(() =>
+                caches.match("./offline.html")
+            )
+        );
+    }
 });
